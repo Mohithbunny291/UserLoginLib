6c20b74263132358506b29d578ba9f925517d16f
Mohith Reddy <Mohithbunny291@gmail.com>
2024-02-18 14:43:57 +0530

    Add project files.

---
+++ SpecFlowProjectUserLogin/Features/UserLogin.feature
@@ -0,0 +1,16 @@
+﻿Feature: UserLogin
+
+A short summary of the feature
+
+
+@mytag
+Scenario: Successfull UserLogin
+	Given a user with valid username "Sam121" and valid password "Sam@121"
+
+	When the Login method is called
+	Then the result should Login Success
+
+Scenario: UnSuccessfull UserLogin
+	Given a user with invalid username "invalid_user" and invalid password "invalid"
+	When the Login method is called
+	Then the result should Login Failed
---
+++ UserLoginLib/UserLogin.cs
@@ -0,0 +1,19 @@
+﻿namespace UserLoginLib
+{
+    public class UserLogin
+    {
+        public string Login(string username, string password)
+        {
+            string result;
+            if(username == "Sam121" &&  password == "Sam@121")
+            {
+                result = "Login Success";
+            }
+            else
+            {
+                result = "Login Failed";
+            }
+            return result;
+        }
+    }
+}
